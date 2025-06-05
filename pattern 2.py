44c19195b5a9f1dbeb69b908bee876c4baaaf9e9
Jinsy Benny <jinsybenny124@gmail.com>
2025-06-05 18:51:44 +0530

    Add files via upload

--- pattern 2.py
+++ pattern 2.py
@@ -1,13 +1,18 @@
+
+# Right Downward Triangle pattern
+
 for i in range(1,6):
     for j in range(0,6-i):
         print("*",end=" ")
     print(" ")
+# Right Triangle Pattern
 
 for j in range(0,6):
     for i in range(0,j+1):
         print("*",end="")
     print(" ")
 
+# Pyramid pattern
 
 n=int(input("enter the no:"))
 for i in range(0,n+1):
@@ -17,6 +22,7 @@ for i in range(0,n+1):
         print("*",end=" ")
     print()    
 
+# Reverse Pyramid  Pattern
 
 n=int(input("enter the no:"))
 for i in range(0,n+1):
